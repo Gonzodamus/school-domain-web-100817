5d330a3a2bc89221d1a9ab61369a799b3b57f9d2
Gonzodamus <Dick@TheDickWard.com>
2017-10-11 14:08:30 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School
+
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+  end
+
+  def name
+    @name
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student_name, grade)
+    @student_name = student_name
+    @grade = grade
+    if roster[@grade] == nil
+      roster[@grade] = [@student_name]
+    else
+      roster[@grade] << @student_name
+    end
+  end
+
+  def grade (year)
+    roster[year]
+  end
+
+  def sort
+    @roster.map {|key, value| [key,value.sort]}.to_h
+  end
+
+end
